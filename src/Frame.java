5b25bd8a1604aa2f24c35bb822b6971c8eb06f40
mishan-tharindu <v.mishantharindu@gmail.com>
2024-01-15 17:55:06 +0530

    Update Listing order

--- src/Frame.java
+++ src/Frame.java
@@ -22,55 +22,44 @@ public class Frame extends JFrame{
         setVisible(true);
 
 
+
         clickHereButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String getText_feild01 = textArea1.getText();
 
+                String encoding = "UTF-8";
 
+                // Array of Sinhala words (including hyphenated words)
+//                String[] sinhalaWords = {
+//                        "නමස්කාරය", "කරුණාකර", "ස්තූතියි", "ආයුබෝවන්",
+//                        "සුබ-උදෑසනක්", "සුබ-දවසක්", "සුබ-රාත්‍රියක්", "ආයුබෝවන්-කරුණාකර", "අම්මා-කාර", "අම්මා-ඇර", "අමමා-ආර"
+//                };
                 String[] sinhalaWords = getText_feild01.split("\n");
 
-                // String[] sinhalaWords = {"මිෂාන්", "නබි", "කබි", "පස", "මිෂ", "මෂ", "මිෂා"};
-                // Creating a Sinhala collator to compare and sort Sinhala words
-                Collator collator = Collator.getInstance(new Locale("si", "LK"));
-                collator.setStrength(Collator.PRIMARY);
+                // Sort the words based on the alphabetical order of all characters in the first word
+                Arrays.sort(sinhalaWords, (word1, word2) -> {
+                    // Extract first words before hyphens
+                    String firstPart1 = word1.split("-")[0];
+                    String firstPart2 = word2.split("-")[0];
 
-                // Sorting the Sinhala words
-//                Arrays.sort(sinhalaWords, collator);
+                    // Compare characters one by one until a difference is found
+                    int n1 = firstPart1.length();
+                    int n2 = firstPart2.length();
+                    for (int i = 0; i < Math.min(n1, n2); i++) {
+                        int comparison = Character.compare(firstPart1.charAt(i), firstPart2.charAt(i));
+                        if (comparison != 0) return comparison;
+                    }
 
-                // Displaying the words in Sinhala alphabetical order
-//                System.out.println("Words in Sinhala alphabetical order:");
+                    // Handle different word lengths
+                    return n1 - n2;
+                });
 
-                ArrayList<String> addWordtoStongArr= new ArrayList<String>();
-                ArrayList<String> addSecondtoStringArr = new ArrayList<String>();
-
-                String[] firstWordArr = new String[0];
-                String[] afterFirstword = new String[0];
-
-
+                // Print the sorted words with proper encoding
                 for (String word : sinhalaWords) {
-
-//                  System.out.println("Index of "+word+" :::"+findIndex(sinhalaWords, word));
-                    String[] sinhalaSplitWords = word.split(" - ",2);
-
-                    addWordtoStongArr.add(sinhalaSplitWords[0]);
-                    addSecondtoStringArr.add(sinhalaSplitWords[1]);
-
-                     firstWordArr = addWordtoStongArr.toArray(new String[0]);
-                    afterFirstword = addSecondtoStringArr.toArray(new String[0]);
-
-//                    System.out.println("String array ::: "+Arrays.toString(firstWordArr));
-
-//                    System.out.println("Frist Split Word ::"+sinhalaSplitWords[0]);
-//                    System.out.println("Second Words ::"+sinhalaSplitWords[1]);
-
-//                    orderedtext.append(word+"\n");
-
+//                    System.out.println(word);
+                    orderedtext.append(word+"\n");
                 }
-
-                System.out.println("String First Word array ::: "+Arrays.toString(firstWordArr));
-                System.out.println("String After First Word array ::: "+Arrays.toString(afterFirstword));
-
 
 
             }
@@ -83,6 +72,91 @@ public class Frame extends JFrame{
 
     }
 
+    public static void secondmethode()
+    {
+        String encoding = "UTF-8";
+
+        // Array of Sinhala words (including hyphenated words)
+//        String[] sinhalaWords = getText_feild01.split("\n");
+
+                String[] sinhalaWords = {
+                        "නමස්කාරය", "කරුණාකර", "ස්තූතියි", "ආයුබෝවන්",
+                        "සුබ-උදෑසනක්", "සුබ-දවසක්", "සුබ-රාත්‍රියක්", "ආයුබෝවන්-කරුණාකර", "අම්මා - කාර", "අම්මා - ඇර", "අමමා - ආර", "ඇය - අම්මා", "01 - ඇය - අම්මා"
+                };
+
+        // Sort the words based on the first word before the hyphen
+        Arrays.sort(sinhalaWords, (word1, word2) -> {
+            // Split words if hyphenated
+            String[] parts1 = word1.split("-");
+            String[] parts2 = word2.split("-");
+            // Compare first words based on Sinhala character order
+            int comparison = Character.compare(parts1[0].charAt(0), parts2[0].charAt(0));
+            if (comparison != 0) return comparison;
+            // If first words are identical, compare remaining parts (if any)
+            if (parts1.length > 1 && parts2.length > 1) {
+                comparison = Arrays.compare(parts1, 1, parts1.length, parts2, 1, parts2.length);
+                if (comparison != 0) return comparison;
+            }
+            // Handle different word lengths and hyphen usage
+            return word1.length() - word2.length();
+        });
+
+        // Print the sorted words with proper encoding
+        for (String word : sinhalaWords) {
+//                    System.out.println(word);
+//            orderedtext.append(word+"\n");
+        }
+    }
+
+
+    public static void oldcode()
+    {
+//
+//                String[] sinhalaWords = getText_feild01.split("\n");
+//
+//                // String[] sinhalaWords = {"මිෂාන්", "නබි", "කබි", "පස", "මිෂ", "මෂ", "මිෂා"};
+//                // Creating a Sinhala collator to compare and sort Sinhala words
+//                Collator collator = Collator.getInstance(new Locale("si", "LK"));
+//                collator.setStrength(Collator.PRIMARY);
+//
+//                // Sorting the Sinhala words
+//        //                Arrays.sort(sinhalaWords, collator);
+//
+//                // Displaying the words in Sinhala alphabetical order
+//        //                System.out.println("Words in Sinhala alphabetical order:");
+//
+//                ArrayList<String> addWordtoStongArr= new ArrayList<String>();
+//                ArrayList<String> addSecondtoStringArr = new ArrayList<String>();
+//
+//                String[] firstWordArr = new String[0];
+//                String[] afterFirstword = new String[0];
+//
+//
+//                for (String word : sinhalaWords) {
+//
+//        //                  System.out.println("Index of "+word+" :::"+findIndex(sinhalaWords, word));
+//                    String[] sinhalaSplitWords = word.split(" - ",2);
+//
+//                    addWordtoStongArr.add(sinhalaSplitWords[0]);
+//                    addSecondtoStringArr.add(sinhalaSplitWords[1]);
+//
+//                    firstWordArr = addWordtoStongArr.toArray(new String[0]);
+//                    afterFirstword = addSecondtoStringArr.toArray(new String[0]);
+//
+//        //                    System.out.println("String array ::: "+Arrays.toString(firstWordArr));
+//
+//        //                    System.out.println("Frist Split Word ::"+sinhalaSplitWords[0]);
+//        //                    System.out.println("Second Words ::"+sinhalaSplitWords[1]);
+//
+//        //                    orderedtext.append(word+"\n");
+//
+//                }
+//
+//                System.out.println("String First Word array ::: "+Arrays.toString(firstWordArr));
+//                System.out.println("String After First Word array ::: "+Arrays.toString(afterFirstword));
+//
+//
+    }
 
     public static int findIndex(String arr[],String t)
     {
